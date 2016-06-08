8ca62a1f2d58cc183f010af11e37193340132629
Chen <riichenchen@gmail.com>
2016-06-08 08:35:49 -0700

    Basic implementation of syscalls: - Assumes a basic file descriptor <--> file API - All syscalls except for exec, wait - Needs implementation of FDs belonging to processes + denying writes to executables - Compiles but is untested

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,10 +1,21 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include <stdbool.h>
+
+int read_arg32(void *ptr);
+
 void syscall_init(void);
 void sys_exit(int status);
 void sys_write(int fd, void *buffer, unsigned size);
-int read_arg32(void *ptr);
+bool sys_create(const char *file, unsigned i_size);
+bool sys_remove(const char *file);
+int sys_filesize(int fd);
+int sys_read(int fd, void *buffer, unsigned size);
+void sys_seek(int fd, unsigned position);
+unsigned sys_tell(int fd);
+int sys_open(const char *file);
+void sys_close(int fd);
 
 #endif /* userprog/syscall.h */
 
